d323260b08c0a1de10fd45ab4c17bc0ee1bda1b2
wuberry <wuy000633@gmail.com>
2025-06-14 16:52:25 +0800

    Create _not-found.jsx

---
+++ app/_not-found.jsx
@@ -0,0 +1,9 @@
+// app/_not-found.jsx
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>404 - 页面未找到</h1>
+      <p>你访问的页面不存在，请检查地址后重试～</p>
+    </div>
+  );
+}
